70aca95fe9719fc6d38414bf88b929e0be1e042d
Santiago López Botero <108085502+slopezb16@users.noreply.github.com>
2024-10-19 23:23:25 -0500

    modificando track para que funcione con la api

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
-import * as dataRaw from '../../../../data/track.json';
+// import * as dataRaw from '../../../../data/track.json';
 import { TrackService } from '@modules/tracks/services/track.service';
 import { Subscription } from 'rxjs';
-import { SectionGenericComponent } from '../../../../shared/components/section-generic/section-generic.component';
 
 @Component({
-    selector: 'app-tracks-page',
-    templateUrl: './tracks-page.component.html',
-    styleUrl: './tracks-page.component.css',
-    standalone: true,
-    imports: [SectionGenericComponent],
+  selector: 'app-tracks-page',
+  templateUrl: './tracks-page.component.html',
+  styleUrl: './tracks-page.component.css',
 })
 export class TracksPageComponent implements OnInit, OnDestroy {
   tracksTrending: Array<TrackModel> = [];
@@ -25,26 +22,76 @@ export class TracksPageComponent implements OnInit, OnDestroy {
     // // console.log(data);
     // this.mockTracksList = data;
     //TODO: vamos a cambiar lo de arriba por un servicio
+    // const observer1$ = this.trackService.dataTracksTrending$.subscribe(
+    //   (response) => {
+    //     this.tracksTrending = response;
+    //     this.tracksRandom = response;
+    //     console.log('Canciones --> ', response);
+    //   }
+    // );
+    // const observer2$ = this.trackService.dataTracksRandom$.subscribe(
+    //   (response) => {
+    //     this.tracksRandom = [...this.tracksRandom, ...response];
+    //     console.log('Cancion rando entrando..👌--> ', response);
+    //   }
+    // );
+    // this.listObsevers$ = [observer1$, observer2$];
+    //Esta sacando erro por el httpcliente de tracksService
+    // this.trackService.getAllTrack$().subscribe((response) => {
+    //   // this.tracksTrending = response;
+    //   // this.tracksRandom = response;
+    //   console.log('Canciones --> ', response);
+    // });
+    // this.trackService.getAllTracks$().subscribe((response) => {
+    //   this.tracksTrending = response;
+    //   // this.tracksRandom = response;
+    //   console.log('Canciones --> ', response);
+    // });
+    // this.trackService.getAllRandom$().subscribe((response) => {
+    //   this.tracksRandom = response;
+    //   console.log('Canciones rampn --> ', response);
+    // });
+    this.loadDataAll();
+    this.loadDataRamdom();
+  }
 
-    const observer1$ = this.trackService.dataTracksTrending$.subscribe(
-      (response) => {
-        this.tracksTrending = response;
+  loadDataAll(): void {
+    // this.trackService.getAllTracks$().subscribe((response: TrackModel[]) => {
+    //   this.tracksTrending = response;
+    //   console.log('Canciones rampn --> ', response);
+    // });
+    this.trackService
+      .getAllTracks$()
+      .toPromise()
+      .then((res) => {
+        this.tracksTrending = res;
+        console.log('Canciones --> ', res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  //asincrono
+  async loadDataAll_(): Promise<any> {
+    const dataRaw = await this.trackService.getAllTracks$().toPromise();
+    this.tracksTrending = dataRaw;
+    console.log('Canciones --> ', dataRaw);
+  }
+
+  loadDataRamdom(): void {
+    this.trackService.getAllRandom$().subscribe(
+      (response: TrackModel[]) => {
         this.tracksRandom = response;
-        console.log('Canciones --> ', response);
+        console.log('Canciones rampn --> ', response);
+      },
+      (err) => {
+        console.error(err);
       }
     );
-
-    const observer2$ = this.trackService.dataTracksRandom$.subscribe(
-      (response) => {
-        this.tracksRandom = [...this.tracksRandom, ...response];
-        console.log('Cancion rando entrando..👌--> ', response);
-      }
-    );
-
-    this.listObsevers$ = [observer1$, observer2$];
   }
 
   ngOnDestroy(): void {
-    this.listObsevers$.forEach((x) => x.unsubscribe());
+    // this.listObsevers$.forEach((x) => x.unsubscribe());
   }
 }
--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
-import { Observable, of } from 'rxjs';
-import * as dataRaw from '../../../data/track.json';
+import { catchError, map, mergeMap, Observable, of, tap } from 'rxjs';
+// import * as dataRaw from '../../../data/track.json';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,31 +11,83 @@ import * as dataRaw from '../../../data/track.json';
 export class TrackService {
   //TODO > A las variables observables se les suele poner un $ al final
   //https://www.learnrxjs.io/
-  dataTracksTrending$: Observable<TrackModel[]> = of([]);
+  // dataTracksTrending$: Observable<TrackModel[]> = of([]);
   //TODO > a los observables hay que suscribirse . Si no no se tiene la data
 
-  dataTracksRandom$: Observable<TrackModel[]> = of([]);
+  // dataTracksRandom$: Observable<TrackModel[]> = of([]);
 
-  constructor() {
-    const { data }: any = (dataRaw as any).default;
-    this.dataTracksTrending$ = of(data);
+  //api
+  private readonly URL = environment.api;
 
+  constructor(private http: HttpClient) {
+    // const { data }: any = (dataRaw as any).default;
+    // this.dataTracksTrending$ = of(data);
     //
-    this.dataTracksRandom$ = new Observable((observer) => {
-      //TODO > nuevo elemento como si fuera dinamico
-      const trackExample: TrackModel = {
-        _id: 9,
-        name: 'Leve',
-        album: 'Cartel de Santa',
-        url: 'http://',
-        cover:
-          'https://www.sanborns.com.mx/imagenes-sanborns-ii/1200/889853882823.jpg',
-      };
+    // this.dataTracksRandom$ = new Observable((observer) => {
+    //   //TODO > nuevo elemento como si fuera dinamico
+    //   const trackExample: TrackModel = {
+    //     _id: 9,
+    //     name: 'Leve',
+    //     album: 'Cartel de Santa',
+    //     url: 'http://',
+    //     cover:
+    //       'https://www.sanborns.com.mx/imagenes-sanborns-ii/1200/889853882823.jpg',
+    //   };
+    //   observer.next([trackExample]);
+    //   // setTimeout(() => {
+    //   //   observer.next([trackExample]);
+    //   // }, 3000);
+    // });
+  }
 
-      observer.next([trackExample]);
-      // setTimeout(() => {
-      //   observer.next([trackExample]);
-      // }, 3000);
+  /**
+   *
+   */
+  private skipById(
+    listTracks: TrackModel[],
+    id: Number
+  ): Promise<TrackModel[]> {
+    return new Promise((resolve, reject) => {
+      const listTmp = listTracks.filter((x) => x._id !== id);
+      resolve(listTmp);
     });
+  }
+
+  /**
+   *TODO: {Data:[dato1,dato2]}
+   *
+   * @returns
+   */
+  getAllTracks$(): Observable<any> {
+    return this.http.get(`${this.URL}/tracks`).pipe(
+      map(({ data }: any) => {
+        return data;
+      })
+    );
+  }
+
+  /**
+   *
+   * @returns Devolver canciones Random
+   */
+  getAllRandom$(): Observable<any> {
+    return this.http.get(`${this.URL}/tracks`).pipe(
+      // return this.http.get(`${this.URL}/tracks_`).pipe(
+      mergeMap(({ data }: any) => this.skipById(data, 2)),
+      // map((response: any) => {
+      //   const dataRevertida = response.data.reverse(); // Invertir la data
+      //   return dataRevertida;
+      // })
+      // map((dataRevertida: TrackModel[]) => {
+      //   // Aplicar filtro para remover el track con _id === 1
+      //   return dataRevertida.filter((track: TrackModel) => track._id !== 1);
+      // })
+      tap((data) => console.log('Ok', data)),
+      catchError((err) => {
+        const { status, statusText } = err;
+        console.log('Error catch', [status, statusText]);
+        return of([]);
+      })
+    );
   }
 }
