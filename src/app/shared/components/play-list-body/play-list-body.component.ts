bc7a301a4d94ee7c8b6e6a5efa41cef75823cf8d
slopezb16 <79761738+slopezb16@users.noreply.github.com>
2025-09-06 16:40:44 -0500

    fix: Ajustes funcionalidades play canciones

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,21 +1,18 @@
 import { Component, Input } from '@angular/core';
-// import * as dataRaw from '../../../data/track.json';
+// import * as dataRaw from '../../../data/track.json'; // Local
+import { TrackService } from '@modules/tracks/services/track.service'; // API
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
 import { NgFor, NgTemplateOutlet } from '@angular/common';
-import { ImgBrokenDirective } from '../../directives/img-broken.directive';
-import { OrderListPipe } from '../../pipe/order-list.pipe';
+import { ImgBrokenDirective } from '@shared/directives/img-broken.directive';
+import { OrderListPipe } from '@shared/pipe/order-list.pipe';
 
 @Component({
-    selector: 'app-play-list-body',
-    templateUrl: './play-list-body.component.html',
-    styleUrl: './play-list-body.component.css',
-    standalone: true,
-    imports: [
-        NgFor,
-        NgTemplateOutlet,
-        ImgBrokenDirective,
-        OrderListPipe,
-    ],
+  selector: 'app-play-list-body',
+  templateUrl: './play-list-body.component.html',
+  styleUrl: './play-list-body.component.css',
+  standalone: true,
+  imports: [NgFor, NgTemplateOutlet, ImgBrokenDirective, OrderListPipe],
 })
 export class PlayListBodyComponent {
   @Input() tracks: Array<TrackModel> = [];
@@ -24,10 +21,35 @@ export class PlayListBodyComponent {
     property: null,
     order: 'asc',
   };
+  constructor(
+    private trackService: TrackService,
+    private multimediaService: MultimediaService // 👈 aquí
+  ) {}
+
+  // ngOnInit(): void {
+  //   const { data }: any = (dataRaw as any).default;
+  //   this.tracks = data;
+  // }
 
   ngOnInit(): void {
-    // const { data }: any = (dataRaw as any).default;
-    // this.tracks = data;
+    this.loadDataAll();
+  }
+
+  loadDataAll(): void {
+    // this.trackService.getAllTracks$().subscribe((response: TrackModel[]) => {
+    //   this.tracksTrending = response;
+    //   console.log('Canciones rampn --> ', response);
+    // });
+    this.trackService
+      .getAllTracks$()
+      .toPromise()
+      .then((res) => {
+        this.tracks = res;
+        console.log('Canciones --> ', res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   changeSort(property: string): void {
@@ -37,5 +59,13 @@ export class PlayListBodyComponent {
       order: order === 'asc' ? 'desc' : 'asc',
     };
     console.log(this.optionSort);
+  }
+
+  // 👉 Método para reproducir
+  // sendPlay(track: TrackModel): void {
+  //   this.multimediaService.trackInfo$.next(track);
+  // }
+  sendPlay(track: TrackModel): void {
+    this.multimediaService.trackInfoSignal.set(track); // 👈 usa el signal, no el BehaviorSubject
   }
 }
